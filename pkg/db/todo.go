f9daaa64f74aa87fc83b2c0d5857b4d5bc333465
Nam Truong Le <nam.truong.le@sap.com>
2022-12-05 21:39:25 +0100

    add db/todo

---
+++ pkg/db/todo.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"context"
+	"time"
+
+	"github.com/nam-truong-le/lambda-utils-go/pkg/mongodb"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+const collectionTodo = "todo"
+
+type Todo struct {
+	ID          primitive.ObjectID `bson:"_id" json:"id"`
+	Group       string             `bson:"group" json:"group"`
+	Description string             `bson:"description" json:"description"`
+	Priority    string             `bson:"priority" json:"priority"`
+	Deadline    *time.Time         `bson:"deadline,omitempty" json:"deadline,omitempty"`
+	Created     time.Time          `bson:"created" json:"created"`
+}
+
+func CollectionTodo(ctx context.Context) (*mongo.Collection, error) {
+	return mongodb.Collection(ctx, collectionTodo)
+}
